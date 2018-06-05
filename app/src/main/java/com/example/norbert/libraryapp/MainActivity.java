e6a46178e347172b1dc084aa90061399cf043169
Norbert Leśniak <norbert.lesniak@gmail.com>
2018-06-05 21:54:06 +0200

    Utworzenie ekranu głównego

--- app/src/main/java/com/example/norbert/libraryapp/MainActivity.java
+++ app/src/main/java/com/example/norbert/libraryapp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.norbert.libraryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bBookList = findViewById(R.id.bBookList );
+        bBookList.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast. makeText (MainActivity. this , "TODO" , Toast. LENGTH_SHORT ).show();
+            }
+        });
     }
 }
